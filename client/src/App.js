030fec374380ef465324b351557de676c0fc79f1
tfujimotowustl <59811628+tfujimotowustl@users.noreply.github.com>
2021-10-02 15:30:12 -0500

    login work

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">
+        Bear Sleuth
+      </h1>
+      <form id="login">
+        <input type="text"></input>
+        <input type="password"></input>
+        <input id="loginBut" type="submit" value="Login"></input>
+      </form>
+      <form id="Register">
+        <p>Don't have an account? Click here</p>
+        <input type="submit" value="Register Here"></input>
+      </form>
     </div>
   );
 }
