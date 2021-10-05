a9b3257f95dd9ef9666c2588d0772c2952bf8686
Ben <bengross@wustl.edu>
2021-10-04 21:42:58 -0500

    loginpage formatting

--- client/src/App.js
+++ client/src/App.js
@@ -25,14 +25,15 @@ function App() {
         Bear Sleuth
       </h1>
       <form id="login">
-        <input id="username" type="text"></input>
-        <input id="password" type="password"></input>
+        <input id="username" type="text" placeholder ="username"></input><span></span>
+        <input id="password" type="password" placeholder ="password"></input><span></span>
         <input id="loginBut" type="submit" value="Login" onClick={login} ></input>
       </form>
       <form id="Register">
         <p>Don't have an account? Click here</p>
         <input type="submit" onClick={newAccount} value="Register Here"></input>
       </form>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTguXSl2UiC7ZSxPiZELnOZjqdSFXzyAwgVhw&usqp=CAU"></img>
     </div>
   );
 }
