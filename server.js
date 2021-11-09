1f41f9c5fc2ba52f7eea7b7dc0a2b7d9925c402e
Ben <bengross@wustl.edu>
2021-11-09 13:05:08 -0600

    undo

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ const server = http.createServer(app);
 
 const io = socketIO(server, {
   cors: true,
-  origins: ["http://ec2-18-223-203-85.us-east-2.compute.amazonaws.com:3000"],
-b
-  //  origins: ["localhost:3000"],
+//  origins: ["http://ec2-18-223-203-85.us-east-2.compute.amazonaws.com:3000"],
+    origins: ["localhost:3000"],
 });
 
 // Hardcoding a room name here. This is to indicate that you can do more by creating multiple rooms as needed.
